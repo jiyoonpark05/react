9f9e0effbbfbf4d756013ee147979ad385f23eb4
박지윤 <parkjiyoon@parkjiyoon-ui-MacBook-Pro.local>
2022-06-24 17:36:32 +0900

    [add] using custom hook

---
+++ react-form-submit/src/components/SimpleInput.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import useInput from "../hooks/use-input";
+
+const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: reseEmailInput,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    // 브라우저가 http 요처을 보내지 않도록함. (페이지가 새로고침 되는 경우 리액트 앱이 모두 재시작되기 때문)
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    resetNameInput();
+    reseEmailInput();
+  };
+
+  const nameInputClasses = nameInputHasError
+    ? "form-control invalied"
+    : "form-control";
+
+  const emailInputClasses = emailInputHasError
+    ? "form-control invalied"
+    : "form-control";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName}
+        />
+      </div>
+      {nameInputHasError && (
+        <p className="error-text">Name must not be empty</p>
+      )}
+      <div className={emailInputClasses}>
+        <label htmlFor="name">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+      </div>
+
+      {emailInputHasError && (
+        <p className="error-text">please enter valid email</p>
+      )}
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
